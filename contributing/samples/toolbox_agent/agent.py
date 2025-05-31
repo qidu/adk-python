12223d84ea98dedafdd13f2b4bf520706e313b80
qidu <qidu@outlook.com>
2025-05-31 23:58:26 +0800

    update toolbox_agent with: - define new tools with SQL statements - add MCP in agent - add tables in db

--- contributing/samples/toolbox_agent/agent.py
+++ contributing/samples/toolbox_agent/agent.py
@@ -12,17 +12,49 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
 from google.adk.tools.toolbox_toolset import ToolboxToolset
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+from google.adk.tools.mcp_tool.mcp_toolset import StdioServerParameters
+
+_allowed_path = os.path.dirname(os.path.abspath(__file__))
+print(f"Allowed path: {_allowed_path}")
 
 root_agent = Agent(
-    model="gemini-2.0-flash",
+    # model="gemini-2.0-flash",
+    model=LiteLlm(model="custom_openai/deepseek-v3"),
+    # model=LiteLlm(model="deepseek/deepseek-chat"),
     name="root_agent",
-    instruction="You are a helpful assistant",
+    instruction=f"You are a helpful assistant, designed to assist with customer service tasks. you can operate files in allowed directory {_allowed_path}.",
     # Add Toolbox tools to ADK agent
     tools=[
         ToolboxToolset(
-            server_url="http://127.0.0.1:5000", toolset_name="my-toolset"
+            server_url="http://127.0.0.1:5000", toolset_name="customer-toolset"
+        ),
+        ToolboxToolset(
+            server_url="http://127.0.0.1:5000", toolset_name="service-toolset"
+        ),
+        MCPToolset(
+            connection_params=StdioServerParameters(
+                command='npx',
+                args=[
+                    '-y',  # Arguments for the command
+                    '@modelcontextprotocol/server-filesystem',
+                    _allowed_path,
+                ],
+            ),
+            tool_filter=[
+                'read_file',
+            #    'read_multiple_files',
+                'list_directory',
+            #    'directory_tree',
+                'search_files',
+                'get_file_info',
+                'list_allowed_directories',
+            ],
         )
     ],
 )
